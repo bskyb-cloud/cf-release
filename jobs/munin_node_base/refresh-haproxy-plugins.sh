4c1e0ac58cf9c77c7cc62a7d3ad86f2f8a102f67
Szlachta, Jacek <jacek.szlachta@sky.uk>
2016-05-25 10:51:54 +0100

    remove munin and nagios jobs and packages - not in use

--- jobs/munin_node_base/refresh-haproxy-plugins.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ls templates/usr/share/munin/plugins/ | while read i; do curl https://raw.githubusercontent.com/munin-monitoring/contrib/master/plugins/haproxy/$i -o templates/usr/share/munin/plugins/$i; done
